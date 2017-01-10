25796f30e95b2aa5a52bdf806ae4728cd5ec02ad
HARISMEHMOOD <haris.mehmood@outlook.com>
2017-01-10 21:38:54 +0400

    u

---
+++ config/module.config.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace AlphaUserSubscription;
+
+return array(
+    'router' => array(
+        'routes' => array(),
+    ),
+    'service_manager' => array(
+        'factories' => array(
+        ),
+    ),
+    'controllers' => array(
+        'factories' => array(
+        ),
+    ),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity')
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        ),
+    ),
+);
---
+++ src/AlphaUserSubscription/Entity/User.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * BjyAuthorize Module (https://github.com/bjyoungblood/BjyAuthorize)
+ *
+ * @link https://github.com/bjyoungblood/BjyAuthorize for the canonical source repository
+ * @license http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace AlphaUserSubscription\Entity;
+
+use AlphaUser\Entity\User;
+use AlphaSubscription\Entity\SubscriptionInterface;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * An example of how to implement a role aware user entity.
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ *
+ * @author Tom Oram <tom@scl.co.uk>
+ */
+class User extends User implements SubscriptionInterface {
+
+    public function getSubscribedFiltersIds($addZeroIfEmpty = true) {
+        $filters_tmp = array();
+
+        $subscriptionColumns = $this->getEntityManager()->getRepository('Alpha\Entity\AlphaStructure')
+                ->findOneBy(['entityName' => 'AlphaSubscription\Entity\Subscription'])
+                ->getColumns();
+
+        $statusField = null;
+
+        foreach ($subscriptionColumns as $column) {
+            if ($column->getColumnName() == 'status')
+                $statusField = $column->getName();
+        }
+
+        $subscriptions = $this->getEntityManager()
+                ->getRepository('AlphaSubscription\Entity\Subscription')
+                ->findBy(array('user' => $this, $statusField => \AlphaSubscription\Entity\Subscription::STATUS_ENABLED));
+
+        foreach ($subscriptions as $subscription) {
+            if ($subscription->getStatus() == \AlphaSubscription\Entity\Subscription::STATUS_ENABLED) {
+                foreach ($subscription->getSubscriptionItem()->getFilters() as $filter) {
+                    $filters_tmp[] = $filter->getId();
+                }
+            }
+        }
+
+        $filters = array_unique($filters_tmp);
+
+        if (empty($filters) && $addZeroIfEmpty)
+            $filters[] = 0;
+
+        return $filters;
+    }
+
+    public function getSubscriptions() {
+        $subscriptions = null;
+
+        $subscriptionColumns = $this->getEntityManager()->getRepository('Alpha\Entity\AlphaStructure')
+                ->findOneBy(['entityName' => 'AlphaSubscription\Entity\Subscription'])
+                ->getColumns();
+
+        $statusField = 'status';
+
+        foreach ($subscriptionColumns as $column) {
+            if ($column->getColumnName() == 'status')
+                $statusField = $column->getName();
+        }
+
+        $subscriptions = $this->getEntityManager()
+                ->getRepository('AlphaSubscription\Entity\Subscription')
+                ->findBy(array('user' => $this, $statusField => \AlphaSubscription\Entity\Subscription::STATUS_ENABLED));
+
+        return $subscriptions;
+    }
+
+}
